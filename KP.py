4f903cb0e81734d2e3cbb8a76af2a1761cfc805f
2131332 <nicolas.idarraga.nieves@gmail.com>
2026-05-11 17:24:16 -0500

    Implement random emoji selection in KP.py
    Add random emoji selection from predefined list.

--- KP.py
+++ KP.py
@@ -1,2 +1,9 @@
 # python -m pip install
 help('modules')
+
+import random
+emojis = ["Me gusta : Dormir y practico lo que vea", "Edad: 12", "Mi nombre: Nicolas"]
+
+for i in range(1):
+    print(random.choice(emojis))
+  
